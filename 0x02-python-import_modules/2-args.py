84ccd6038a746a9e184ab71f246ac620dc53434f
Alemayew Abate <alemayew.abonesh07@gmail.com>
2022-06-30 22:00:45 +0300

    2arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-
-def main(argv):
-
-    if len(argv) - 1 == 0:
-        print("0 arguments.")
-    elif len(argv) - 1 == 1:
-        print("{:d} argument:".format(len(argv) - 1))
-        print("{:d}: {}".format(len(argv) - 1, argv[1]))
-    else:
-        print("{:d} arguments:".format(len(argv) - 1))
-        for i, x in enumerate(argv[1:], 1):
-            print("{:d}: {}".format(i, x))
-
 if __name__ == "__main__":
     import sys
+    """ number of arguments without the file name """
+    arguments = len(sys.argv) - 1
+    if arguments == 0:
+        print("0 arguments.")
+    elif arguments == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arguments))
+    for count in range(arguments):
+        print("{}: {}".format(count + 1, sys.argv[count + 1]))
