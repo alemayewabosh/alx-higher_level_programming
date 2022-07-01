4e27307ff79cc1d5ffddecc98083003c390bf407
Alemayew Abate <alemayew.abonesh07@gmail.com>
2022-07-01 06:02:54 +0300

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
     import hidden_4
-    for i in dir(hidden_4):
-        if (i[:2] != '__'):
-            print(i)
+
+    names_def = dir(hidden_4)
+    for name in names_def:
+        if name[:2] != "__":
+            print(name)
